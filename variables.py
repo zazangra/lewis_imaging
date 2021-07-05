753fb9f3b6d2ee6de2e11e689b5aa748f1f48e7a
Zazangra <alessandro.zangra@edu.unito.it>
2021-07-05 13:48:13 +0200

    removed bugs in build_prob

--- variables.py
+++ variables.py
@@ -18,6 +18,16 @@ class Var():
             par_names.append(i.name)
         return par_names
 
+    def build_cond_string(self):
+        """catch the right string for getting cond prob"""
+        par_names = self.build_parname()
+        string_cond = str(par_names)+"|"+self.name
+        string_cond= string_cond.replace("[","")
+        string_cond= string_cond.replace("]","")
+        string_cond= string_cond.replace("'","")
+        string_cond= string_cond.replace(" ","")
+        return str(string_cond)
+
     def build_prob(self):
         """Build probability dictionary """
 
@@ -28,28 +38,16 @@ class Var():
         else:
             par_names = self.build_parname()
             conf = prod([0,1], repeat=len(self.par))
-            string_cond = str(par_names)+"|"+self.name
-            string_cond= string_cond.replace("[","")
-            string_cond= string_cond.replace("]","")
-            string_cond= string_cond.replace("'","")
-            string_cond= string_cond.replace(" ","")
-            print(string_cond)
             for i in conf:
                 string_one = "Tprob "
                 good_i = list(i)
-                #print(good_i)
-                #print(par_names)
                 for j,k in zip(good_i, par_names):
-                   # print(k)
                     string_one += k + "=" + str(j)+" "
                 string_two = string_one.replace("Tprob ", "")
-                string_input="Which is probability of "+string_cond+" if "
+                string_input="Which is probability of "+self.build_cond_string()+" if "
                 self.prob[string_one] = float(input(string_input+string_two+" ?"))
                 string_three = "Fprob "+string_two
                 self.prob[string_three]= 1-self.prob[string_one]
-
-#            self.prob = { #this is going to be huge
-#           }
 
     def stampa(self):
         """Debug"""
