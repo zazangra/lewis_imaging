c48e1f36b7bdde6da806c4844484402e60b17284
zazangra <alessandro.zangra@edu.unito.it>
2021-07-17 19:27:49 +0200

    function input to formula in imaging

--- imaging/imaging_funcs.py
+++ imaging/imaging_funcs.py
@@ -1,15 +1,35 @@
 """We will select worlds in which a given formula happens"""
 from world.create_worlds import universe
+from world.var.var_class import re
+
 #maybe import from main is safer?
 
 n_ant_worlds=[]
 ant_worlds=[]
+
+
 def valuations():
+    formula_instring= input_to_formula()
     for i in universe:
-        if (i.world['a']==0 or i.world['b']==0) and i.world['c']==1:
+        if eval(formula_instring):
             ant_worlds.append(i)
         else:
             n_ant_worlds.append(i)
     return ant_worlds
-  
-      # insert the formula, e. g. if the formula is (a=0 \land b=0) \lor c=0 then write (i.world['a']==0 or i.world['b']==0) and i.world['c']==1
+
+def input_to_formula():
+    """Transforming the input to correct formula string"""
+    pattern_instring='[\s\S](?==)[\s\S](?<==)[\s\S]'
+    string_formula= input('Tell me parents configuration of your counterfactual: ')
+    repla= re.findall(pattern_instring, string_formula)
+    print('this is repla') 
+    print(repla)
+    for k in repla:
+        print('this is k')
+        print(k)
+        print('this is replacement')
+        replacement='i.world[\''+k[0]+'\']=='+k[2]
+        print(replacement)
+        string_formula = string_formula.replace(k, replacement)
+    return string_formula
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from world.var.inputs import create_var, create_par, variables
 from world.create_worlds import universe, world_creator, prob_get
 from world.worlds_class import build_var_names
+from imaging.imaging_funcs import valuations, ant_worlds, n_ant_worlds
 
 create_var()
 create_par()
@@ -22,3 +23,11 @@ for i in universe:
     i.build_world_prob()
     print(i.p_world)
     print(i.prob)
+
+valuations()
+print('this is antworlds')
+for i in ant_worlds:
+    print(i.world)
+print('this is nantworlds')
+for i in n_ant_worlds:
+    print(i.world)
